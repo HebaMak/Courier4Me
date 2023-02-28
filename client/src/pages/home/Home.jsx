33df577b69ae9fc1e9da681d37454ba09c7b9372
HebaSalem <puupiiiii@gmail.com>
2023-02-28 17:24:29 +0100

    upate json files

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import Footer from "../../components/footer/Footer";
@@ -28,34 +27,3 @@ const Home = () => {
 };
 
 export default Home;
-||||||| empty tree
-=======
-import React from "react";
-
-import Footer from "../../components/footer/Footer";
-import FirstSec from "../../components/homepage_sections/FirstSec";
-import Services from "../../components/homepage_sections/Services";
-import AppWorks from "../../components/homepage_sections/AppWorks.jsx";
-import WhyUs from "../../components/homepage_sections/WhyUs";
-import { Helmet } from "react-helmet";
-
-
-const Home = () => {
-  return (
-    <>
-      <Helmet>
-        <title>COURIER4ME | Home</title>
-      </Helmet>
-      <div>
-        <FirstSec />
-        <Services />
-        <AppWorks />
-        <WhyUs />
-        <Footer />
-      </div>
-    </>
-  );
-};
-
-export default Home;
->>>>>>> 11688a079a51568b816f7018dce8dff333938e58
